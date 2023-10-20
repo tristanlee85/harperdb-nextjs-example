66ca7773a1ae7339081dc01abc418ba123339b78
Kris Zyp <kriszyp@gmail.com>
2023-10-20 06:05:18 -0600

    Updates for HDB API

--- resources.js
+++ resources.js
@@ -1,27 +1,17 @@
 const { parse } = require('url')
 const next = require('next')
- 
-const dev = process.env.NODE_ENV !== 'production'
+
+// currently non-dev mode throws errors about missing .next/BUILD_ID and manifest file
+const dev = true;//process.env.NODE_ENV !== 'production'
 const app = next({ dev, dir: __dirname })
 const handle = app.getRequestHandler()
- 
-app.prepare().then(() => {
-  console.log('prepared')
-  server.http(async (request) => {
-    try {
-      const req = request.nodeRequest;
-      const res = request.nodeResponse;
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      await handle(req, res, parsedUrl)
-      return {
-        done: true,
-      }
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
-  });
+
+app.prepare(); // This returns a promise, we could maybe await this
+server.http((request) => {
+  const req = request._nodeRequest;
+  const res = request._nodeResponse;
+  // Be sure to pass `true` as the second argument to `url.parse`.
+  // This tells it to parse the query portion of the URL.
+  const parsedUrl = parse(req.url, true)
+  return handle(req, res, parsedUrl)
 });
